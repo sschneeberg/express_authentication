35636885f17005638be6310a7ffafaf241431563
sschneeberg <simone.m.schneeberg@gmail.com>
2020-11-23 15:54:03 -0500

    Add sign up route

--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,35 @@
 const express = require('express');
 const router = express.Router();
+const db = require('../models');
+const passport = require('../config/ppConfig');
 
 router.get('/signup', (req, res) => {
-  res.render('auth/signup');
+    res.render('auth/signup');
 });
 
 router.get('/login', (req, res) => {
-  res.render('auth/login');
+    res.render('auth/login');
 });
+
+//POST 
+router.post('/signup', (req, res) => {
+    db.user.findOrCreate({
+        where: { email: req.body.body },
+        defaults: { name: req.body.name, password: req.body.password }
+    }).then(([user, created]) => {
+        if (created) {
+            //redirect to home
+            console.log(user.name, ' created')
+            passport.authenticate('local', {
+                successRedirect: '/',
+                successFlash: 'Account created'
+            })(req, res);
+        } else {
+            //already have an exsiting account
+            req.flash('error', 'Account already exists for this email')
+            res.redirect('/auth/signup');
+        }
+    }).catch(err => console.log(err))
+})
 
 module.exports = router;
