06296184f7097880deb3aec73e51e763cfc1fec5
sschneeberg <simone.m.schneeberg@gmail.com>
2020-11-23 12:27:14 -0500

    Update password protection

--- models/user.js
+++ models/user.js
@@ -38,12 +38,26 @@ module.exports = (sequelize, DataTypes) => {
         sequelize,
         modelName: 'user',
     });
+
+    //password protection
+    user.addHook('beforeCreate', (pendingUser) => {
+        //Bcrypt make hash password
+        let hashPassword = bcrypt.hashSync(pendingUser.password, 12);
+        //replace password with hash
+        pendingUser.password = hashPassword;
+    })
+
+    //validate user when logging in
+    user.prototype.validPassword = function(passwordTyped) {
+        let correctPassword = bcrypt.compareSync(passwordTyped, this.password)
+    }
+
+    //remove password before it get serialized in req
+    user.prototype.toJSON = function() {
+        let userData = this.get();
+        delete userData.password;
+        return userData;
+    }
+
     return user;
 };
-
-user.addHook('beforeCreate', (pendingUser) => {
-    //Bcrypt make hash password
-    let hashPassword = bcrypt.hashSync(pendingUser.password, 12);
-    //replace password with hash
-    pendingUser.password = hashPassword;
-})
