f78a08bce1f2e5b59c12d91673504c9ac5cc0436
sschneeberg <simone.m.schneeberg@gmail.com>
2020-11-23 12:50:40 -0500

    Set up Session

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ require('dotenv').config();
 const express = require('express');
 const layouts = require('express-ejs-layouts');
 const app = express();
+const session = require('express-session');
+const passport = require('./config/ppConfig');
+const flash = require('connect-flash');
+const secret_session = process.env.SECRET_SESSION;
+
+console.log(secret_session);
 
 app.set('view engine', 'ejs');
 
@@ -10,12 +16,18 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static(__dirname + '/public'));
 app.use(layouts);
 
+const sessionObj = {
+    secret: secret_session,
+    resave: false,
+    saveUninitialized: true
+}
+
 app.get('/', (req, res) => {
-  res.render('index');
+    res.render('index');
 });
 
 app.get('/profile', (req, res) => {
-  res.render('profile');
+    res.render('profile');
 });
 
 app.use('/auth', require('./routes/auth'));
@@ -23,7 +35,7 @@ app.use('/auth', require('./routes/auth'));
 
 const PORT = process.env.PORT || 3000;
 const server = app.listen(PORT, () => {
-  console.log(`🎧 You're listening to the smooth sounds of port ${PORT} 🎧`);
+    console.log(`🎧 You're listening to the smooth sounds of port ${PORT} 🎧`);
 });
 
 module.exports = server;
